9aacd6dcbd1133d3b940992446928565c72fa732
Aayush Goyal <aayush.goyal54@gmail.com>
2020-04-01 12:28:20 +0530

    update

--- manual/codes/ee18btech11001/ee18btech11001.py
+++ manual/codes/ee18btech11001/ee18btech11001.py
@@ -17,3 +17,39 @@ Slopes = np.array([0,-20,-60,-40,0,-40,-60])
 Np,Nz = CalculatePolesAndZeros(Slopes)
 print("Number of Poles : " , Np)
 print("Number of Zeros : " , Nz)
+
+def getTransferFunction(Slopes):
+    dSlopes = Slopes[1:]-Slopes[:-1]
+    Num = []
+    Den = []
+    for i in range(len(dSlopes)):
+        if dSlopes[i]%20!=0:
+            return None
+        if dSlopes[i]>=0 :
+            Num += [-10**(i+1)]*(dSlopes[i]//20)
+        else:
+            Den += [-10**(i+1)]*(-dSlopes[i]//20)
+        
+    
+    return np.array(Num),np.array(Den)
+Num,Den =  getTransferFunction(Slopes)
+num = np.poly(Num)
+den = np.poly(Den)
+from scipy import signal
+import matplotlib.pyplot as plt
+s1 = signal.lti(num, den) 
+
+w, mag, phase = signal.bode(s1)
+plt.figure()
+plt.xlabel("f")
+plt.ylabel("H(f)")
+plt.title("Bode Plot")
+plt.semilogx(w, mag)    # Bode magnitude plot
+
+s1 = signal.ZerosPolesGain(Num, Den ,1 )
+w, H = signal.freqresp(s1)
+plt.figure()
+plt.plot(H.real, H.imag, "b")
+plt.plot(H.real, -H.imag, "r")
+plt.title("Nyquist Plot")
+plt.show()
